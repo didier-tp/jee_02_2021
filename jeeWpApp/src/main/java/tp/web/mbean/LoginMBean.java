06a0104932f32c9080f6578f6e1681f6af97e24d
didier <didier@d-defrance.fr>
2021-02-02 13:44:36 +0100

    LoginMBean (debut)

---
+++ jeeWpApp/src/main/java/tp/web/mbean/LoginMBean.java
@@ -0,0 +1,41 @@
+package tp.web.mbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean //ManagedBean de JSF
+@RequestScoped //ou SessionScoped
+public class LoginMBean {
+	
+	private Long numClient; //ou Integer
+	private String password;
+	
+	public String login() {
+		String suite=null;
+		//simulation test mot de passe :
+		if(password.equals("pwd" + numClient)) {
+		    suite="comptes"; //pour naviguer vers comptes.xhtml si login ok
+		}
+		return suite;
+	}
+	
+	public LoginMBean() {
+		super();
+	}
+	
+	public Long getNumClient() {
+		return numClient;
+	}
+	public void setNumClient(Long numClient) {
+		this.numClient = numClient;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
