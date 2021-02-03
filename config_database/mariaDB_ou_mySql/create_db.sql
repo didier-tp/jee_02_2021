b64afdf8b391b95ee737417577ad54c1e69fa864
didier <didier@d-defrance.fr>
2021-02-03 15:34:11 +0100

    base mysql avec table compte

--- config_database/mariaDB_ou_mySql/create_db.sql
+++ config_database/mariaDB_ou_mySql/create_db.sql
@@ -3,9 +3,16 @@ CREATE DATABASE IF NOT EXISTS myDB;
 USE myDB;
 
 DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Compte;
 
 CREATE TABLE User(
     id integer auto_increment,
 	nom VARCHAR(64),
 	password VARCHAR(64),
-	PRIMARY KEY(id));	 
+	PRIMARY KEY(id));
+
+CREATE TABLE Compte(
+    numero integer auto_increment,
+	label VARCHAR(64),
+	solde double,
+	PRIMARY KEY(numero));	
--- config_database/mariaDB_ou_mySql/insert_db.sql
+++ config_database/mariaDB_ou_mySql/insert_db.sql
@@ -8,10 +8,16 @@ USE myDB;
 INSERT INTO User(id,nom,password) VALUES (1,'titi','pwd');
 INSERT INTO User(id,nom,password) VALUES (2,'toto','pwd');
     
+INSERT INTO Compte(numero,label,solde) VALUES (1,'compte 1',600);
+INSERT INTO Compte(numero,label,solde) VALUES (2,'compte 2',500);
+INSERT INTO Compte(numero,label,solde) VALUES (3,'compte 3',200);
+
+    
 
 ###################### VERIFICATIONS ###########################################
 show tables;
 SELECT * FROM User;
+SELECT * FROM Compte;
 
 
 
