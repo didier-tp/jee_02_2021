329618950f98d71d6da122fa87b76c2d828faef9
didier <didier@d-defrance.fr>
2021-02-05 13:54:16 +0100

    debut ws rest

--- jeeWpApp/src/main/java/tp/web/rest/CompteRestWS.java
+++ jeeWpApp/src/main/java/tp/web/rest/CompteRestWS.java
@@ -7,7 +7,9 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import tp.core.bs.CompteService;
 import tp.core.dto.CompteDto;
@@ -21,13 +23,28 @@ public class CompteRestWS {
 	@Inject
 	private CompteService compteService;
 	
+	//URL=http://localhost:8080/jeeWpApp/rest/api-bank/compte/1
+	@GET
+	@Path("{id}")
+	public CompteDto getCompteByNum(@PathParam("id")Long numCpt) {
+		Compte cptEntity = compteService.rechercherCompteSelonNum(numCpt);
+		return new CompteDto(cptEntity.getNumero(),
+		                     cptEntity.getLabel(),
+		                     cptEntity.getSolde());
+	}
 	
 	//URL=http://localhost:8080/jeeWpApp/rest/api-bank/compte
+	//ou http://localhost:8080/jeeWpApp/rest/api-bank/compte?numClient=1
 	@GET
 	@Path("")
-	public List<CompteDto> getAllComptes(){
+	public List<CompteDto> getComptesByCriteria(@QueryParam("numClient")Long numClient){
 		List<CompteDto> listeCompteDto = new ArrayList<>();
-		List<Compte> listeCpt = compteService.rechercherTousLesComptes();
+		List<Compte> listeCpt = null;
+		System.out.println("numClient="+numClient);
+		if(numClient==null)
+			listeCpt=compteService.rechercherTousLesComptes();
+		else 
+			listeCpt=compteService.rechercherComptesDuClient(numClient);
 		for(Compte cptEntity : listeCpt) {
 			listeCompteDto.add(new CompteDto(cptEntity.getNumero(),
 		                                     cptEntity.getLabel(),
