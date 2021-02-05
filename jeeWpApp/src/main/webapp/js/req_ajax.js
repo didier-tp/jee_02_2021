8c62c7387e3874c4914f25485025b58581f0c415
didier <didier@d-defrance.fr>
2021-02-05 15:28:47 +0100

    ajax

--- jeeWpApp/src/main/webapp/js/req_ajax.js
+++ jeeWpApp/src/main/webapp/js/req_ajax.js
@@ -9,7 +9,8 @@ function callbackListeCompte(data){
 	console.log("data (reponse)="+data);
 	var listeComptes = JSON.parse(data);  //conversion de JSON vers objet javascript
 	
-	var zoneTBody = document.getElementById("tBody");
+	var zoneTBody = document.getElementById("tbody");
+	zoneTBody.innerHTML=""; //vider le tableau avant de le re-remplir
 	
 	for(let i=0;i<listeComptes.length;i++){
 		var compte = listeComptes[i];
